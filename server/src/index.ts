13d8e31f91a44c759cd6d9c9aa130ca8cb048a2f
Yousef <yousefhamedhassan47@gmail.com>
2025-08-05 22:10:02 +0300

    Spaghetti

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Express + TypeScript Server!');
+});
+
+app.listen(port, () => {
+  // A little splash of color in the console log!
+  console.log(`\x1b[32m[server]: Server is running at http://localhost:${port}\x1b[0m`);
+});
