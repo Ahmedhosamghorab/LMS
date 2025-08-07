aedfdc80b49747412a274b1ece62bfa6540059cb
Yousef <yousefhamedhassan47@gmail.com>
2025-08-07 13:14:44 +0300

    EL Host MENOWAR YA BTA3 MA3LOL

---
+++ client/src/Routes/Routes.tsx
@@ -0,0 +1,9 @@
+import StudentDashboard from "@/Pages/StudentDashboard";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <StudentDashboard/>
+    },
+]);
